bb87a04e6de4eaf5aa4ebce115347e4c14811dac
haneyman <mark.haney@johnmuirhealth.com>
2020-10-11 10:20:52 -0700

    made buttons more FLEXible :)

--- src/App.js
+++ src/App.js
@@ -10,24 +10,30 @@ import { Button, Row, Col } from 'react-materialize';
 function App() {
   return (
     <div className="container md">
-    
+
       <div className="header">
         <h2 className="header-text">
           Mark's Site
         </h2>
       </div>
+      { /*
       <Row className="buttons">
         <Col s={4}>
-          <Button className="waves-effect waves-light btn" onClick={ () => { window.open("https://ambientweather.net/dashboard/8c8ef06c0f8bebfee287bbe57b4c060f", "_blank")} }>Weather Dashboard</Button>
+          <Button className="waves-effect waves-light btn" onClick={() => { window.open("https://ambientweather.net/dashboard/8c8ef06c0f8bebfee287bbe57b4c060f", "_blank") }}>Weather Dashboard</Button>
         </Col>
         <Col s={4}>
-          <Button className="waves-effect waves-light btn" onClick={ () => { window.open("https://calendar.google.com/calendar/embed?src=c5k8fgk8j5tuoc69ncgahq4hmk%40group.calendar.google.com&ctz=America%2FLos_Angeles", "_blank")} }>Cabin Calendar</Button>
-        </Col>
-        <Col s={3}>
-        </Col>
-        <Col s={3}>
+          <Button className="waves-effect waves-light btn" onClick={() => { window.open("https://calendar.google.com/calendar/embed?src=c5k8fgk8j5tuoc69ncgahq4hmk%40group.calendar.google.com&ctz=America%2FLos_Angeles", "_blank") }}>Cabin Calendar</Button>
         </Col>
       </Row>
+      */ }
+      <div className="div-buttons">
+        <div className="div-button" onClick={() => { window.open("https://ambientweather.net/dashboard/8c8ef06c0f8bebfee287bbe57b4c060f", "_blank") }}>
+          Weather Dashboard
+        </div>
+        <div className="div-button" onClick={() => { window.open("https://calendar.google.com/calendar/embed?src=c5k8fgk8j5tuoc69ncgahq4hmk%40group.calendar.google.com&ctz=America%2FLos_Angeles", "_blank") }}>
+          Cabin Calendar
+        </div>
+      </div>
     </div>
   );
 }
